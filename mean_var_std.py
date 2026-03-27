9cde6814ce2da982f40e92ca1f0a9d5369db6ea5
areeves <areeves@users.noreply.github.com>
2026-03-27 12:06:11 -0400

    Add mean calculation to the statistics function

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,10 @@
 import numpy as np
 
 def calculate(list):
-
-
+    mat = np.array(list).reshape(3,3)
+    calculations = {
+        "mean": [ mat.mean(), mat.mean(axis=1), mat.flatten().mean() ]
+    }
 
 
     return calculations
