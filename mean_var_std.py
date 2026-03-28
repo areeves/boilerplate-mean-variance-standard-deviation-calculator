ac717a0d6768785ddb3f7ba5b5d9b60194fd231d
Andrew Reeves <areeves42@gmail.com>
2026-03-28 18:31:17 -0400

    Add input validation and reformat output

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,14 +1,42 @@
 import numpy as np
 
 def calculate(list):
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
+
     mat = np.array(list).reshape(3,3)
+
     calculations = {
-        "mean": [ mat.mean(axis=0), mat.mean(axis=1), mat.flatten().mean() ],
-        "variance": [ mat.var(axis=0), mat.var(axis=1), mat.flatten().var() ],
-        'standard deviation': [ mat.std(axis=0), mat.std(axis=1), mat.flatten().std() ],
-        'max': [ mat.max(axis=0), mat.max(axis=1), mat.flatten().max() ],
-        'min': [ mat.min(axis=0), mat.min(axis=1), mat.flatten().min() ],
-        'sum': [ mat.sum(axis=0), mat.sum(axis=1), mat.flatten().sum() ]
+        "mean": [
+            mat.mean(axis=0).tolist(),
+            mat.mean(axis=1).tolist(),
+            float(mat.flatten().mean())
+        ],
+        "variance": [
+            mat.var(axis=0).tolist(),
+            mat.var(axis=1).tolist(),
+            float(mat.flatten().var())
+        ],
+        'standard deviation': [
+            mat.std(axis=0).tolist(),
+            mat.std(axis=1).tolist(),
+            float(mat.flatten().std())
+        ],
+        'max': [
+            mat.max(axis=0).tolist(),
+            mat.max(axis=1).tolist(),
+            float(mat.flatten().max())
+        ],
+        'min': [
+            mat.min(axis=0).tolist(),
+            mat.min(axis=1).tolist(),
+            float(mat.flatten().min())
+        ],
+        'sum': [
+            mat.sum(axis=0).tolist(),
+            mat.sum(axis=1).tolist(),
+            float(mat.flatten().sum())
+        ]
     }
 
     return calculations
